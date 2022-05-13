8999eed6adf326506172f54d6a6c0f8edbd525a4
yoursmengle <yoursmengle@126.com>
2022-05-13 12:05:26 +0800

    add update script

---
+++ update_ramdisk.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cd ~/buildroot
+make all
+
+ls -al output/images/rootfs.tar
+mkdir rootfs
+cd rootfs
+cp ~/buildroot/output/images/rootfs.tar .
+tar xvf rootfs.tar
+chmod +x ./etc/init.d/rcS
+rm rootfs.tar
+cd ..
+genext2fs -b 32768 -N 30000 -d rootfs ramdisk
+lzop -9 ramdisk
+mv ramdisk.lzo /mnt/c/tftp
+rm ./rootfs -rf
+ls -al /mnt/c/tftp/ramdisk.lzo
+
+
